1227508098ab4f5abba1bbaee18b6eb32c7da357
smorzechowski <smorzechowski@g.harvard.edu>
2024-08-21 10:30:03 -0400

    Making initial project for R scripts

---
+++ classify_contigs_cyan_synteny_hap1.R
@@ -0,0 +1,168 @@
+# Classify WEHE contigs from HiFi based on coverage and heterozygosity
+# Sophie MacRae Orzechowski
+
+# August 2024
+
+library(dplyr)
+library(ggplot2)
+library(stringr)
+
+setwd("~/PhD research/Neo sex chromosome/WEHE pop gen chapter/WEHE assembly/findZX/Nesoptilotis_hap1")
+
+
+# get list of autosomal contigs to estimate median depth for each sex
+# https://stackoverflow.com/questions/67192258/cant-read-txt-in-r-with-hash-tag-in-the-txt-file
+
+lengths <- read.table("coverage/nleucotis_yamma_366917_f.hap1.p_ctg.filter.10000.fasta.fai",header=F,sep="\t",comment.char="")
+
+matches <- read.table("synteny_lastal/E_cyanotis/bestMatch.list",header=F,sep="\t",comment.char = "")
+
+# Look at the first twenty autosomes from Blue-faced Honeyeater
+
+
+autosome_list <- c("scaffold_1","scaffold_3","scaffold_4","scaffold_6",
+                   "scaffold_7","scaffold_8","scaffold_9","scaffold_10",
+                   "scaffold_11","scaffold_12","scaffold_13","scaffold_14",
+                   "scaffold_15","scaffold_16","scaffold_17","scaffold_18",
+                   "scaffold_19","scaffold_20","scaffold_21","scaffold_22")
+
+
+# Curate a list of contigs that are homologous to these autosomes in Zebra Finch
+
+matches_auto <- matches[matches$V8%in%autosome_list,]
+matches_auto$window_hits <- matches_auto$V3 - matches_auto$V2
+matches_auto_order <- matches_auto[order(matches_auto$V1),]
+matches_auto_order_nondup <- matches_auto_order[!duplicated(matches_auto_order$V1),]
+
+
+matches_auto_summary <- matches_auto %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_auto_summary <- left_join(matches_auto_summary,lengths)
+
+
+# Curate a list of contigs that are homologous to neo-Z in Blue-faced Honeyeater
+
+matches_neoZ <- matches[matches$V8=="scaffold_2",]
+matches_neoZ$window_hits <- matches_neoZ$V3 - matches_neoZ$V2
+matches_neoZ_order <- matches_neoZ[order(matches_neoZ$V1),]
+matches_neoZ_order_nondup <- matches_neoZ_order[!duplicated(matches_neoZ_order$V1),]
+
+matches_neoZ_summary <- matches_neoZ %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_neoZ_summary <- left_join(matches_neoZ_summary,lengths)
+matches_neoZ_summary$percent_match <- matches_neoZ_summary$windowsum/matches_neoZ_summary$V2
+# Designate a threshold for number of windows with hits, or percentage match
+matches_neoZ_final <- matches_neoZ_summary[matches_neoZ_summary$windowsum>35000,]
+matches_neoZ_final <- matches_neoZ_summary[matches_neoZ_summary$percent_match>0.10,]
+
+
+
+
+# Curate a list of contigs that are homologous to neo-W in Blue-faced Honeyeater
+
+matches_neoW <- matches[matches$V8=="scaffold_5",]
+matches_neoW$window_hits <- matches_neoW$V3 - matches_neoW$V2
+matches_neoW_order <- matches_neoW[order(matches_neoW$V1),]
+matches_neoW_order_nondup <- matches_neoW_order[!duplicated(matches_neoW_order$V1),]
+
+
+matches_neoW_summary <- matches_neoW %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_neoW_summary <- left_join(matches_neoW_summary,lengths)
+matches_neoW_summary$percent_match <- matches_neoW_summary$windowsum/matches_neoW_summary$V2
+# Designate a threshold for number of windows with hits or percent match
+matches_neoW_final <- matches_neoW_summary[matches_neoW_summary$windowsum>10000,]
+matches_neoW_final <- matches_neoW_summary[matches_neoW_summary$percent_match>0.10,]
+
+
+##### Estimate coverage for each genomic compartment
+
+
+coverage <- read.table("coverage/gencov.mismatch.0.0.out",header=F,sep="\t",comment.char = "")
+
+# Calculate median depth for selected 20 autosomes for each sex
+
+auto_coverage <- coverage[coverage$V1%in%matches_auto_order_nondup$V1,]
+
+# females
+median_sampleV4 <- median(auto_coverage$V4)
+
+# males
+median_sampleV5 <- median(auto_coverage$V5)
+
+# calculate log2FM following Schield et al. 2022
+
+coverage$Fnorm <- coverage$V4/median_sampleV4
+coverage$Mnorm <- coverage$V5/median_sampleV5
+
+coverage$log2FM <- log2(coverage$Fnorm/coverage$Mnorm)
+
+#### neo-Z coverage calculations ####
+
+# Subset coverage based on window hits to Chr 5
+coverage_neoZ <- coverage[coverage$V1%in%matches_neoZ_final$V1,]
+coverage_neoZ$contig <- str_remove_all(coverage_neoZ$V1,"nleucotis_yamma_366917_f.hap1#pri#")
+
+
+ggplot(coverage_neoZ,aes(contig,log2FM))+
+  geom_boxplot()
+
+
+neoZ <- c("nleucotis_yamma_366917_f.hap1#pri#h1tg000022l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000024l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000079l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000137l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000332l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg001159l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg001300l")
+               #"nleucotis_yamma_366917_f.hap1#pri#h1tg000028l")
+
+neoZ_lengths <- lengths[lengths$V1%in%neoZ,]
+neoZ_hap1 <- sum(neoZ_lengths$V2)
+neoZ_hap1
+
+
+#### neo-W coverage calculations ####
+
+# Subset coverage based on window hits to Chr W
+coverage_neoW <- coverage[coverage$V1%in%matches_neoW_final$V1,]
+
+coverage_neoW$contig <- str_remove_all(coverage_neoW$V1,"nleucotis_yamma_366917_f.hap1#pri#")
+
+ggplot(coverage_neoW,aes(contig,log2FM))+
+  geom_boxplot()+
+  theme(axis.text.x=element_text(angle=45))
+
+chrW_hits <- c("nleucotis_yamma_366917_f.hap1#pri#h1tg000028l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000031l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000037l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000072l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000112l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg000251l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg001630l",
+               "nleucotis_yamma_366917_f.hap1#pri#h1tg002045l")
+chrW_hits_lengths <- lengths[lengths$V1%in%chrW_hits,]
+neoW_hap1 <- sum(chrW_hits_lengths$V2)
+neoW_hap1
+
+#### Calculate mean depth and log2FM depth #####
+
+summary <- coverage %>% select(V1,Fnorm,Mnorm,V4,V5) %>%
+  group_by(V1) %>%
+  summarize(meanFnorm = mean(Fnorm,na.rm=TRUE),
+            meanMnorm = mean(Mnorm,na.rm=TRUE),
+            meanF = mean(V4,na.rm=TRUE),
+            meanM = mean(V5,na.rm=TRUE))%>%
+  as.data.frame()
+
+
+summary$log2FM <- log2(summary$meanF/summary$meanM)
---
+++ classify_contigs_tgut_synteny_primary.R
@@ -0,0 +1,255 @@
+# Classify WEHE contigs from HiFi based on coverage and heterozygosity
+# Sophie MacRae Orzechowski
+
+# August 2024
+
+library(dplyr)
+library(ggplot2)
+library(stringr)
+setwd("~/PhD research/Neo sex chromosome/WEHE pop gen chapter/WEHE assembly/findZX/Nesoptilotis_primary")
+
+
+# get list of autosomal contigs to estimate median depth for each sex
+# https://stackoverflow.com/questions/67192258/cant-read-txt-in-r-with-hash-tag-in-the-txt-file
+
+lengths <- read.table("coverage/nleucotis_yamma_366917_f.p_ctg.filter.10000.fasta.fai",header=F,sep="\t",comment.char="")
+
+matches <- read.table("synteny_lastal/T_guttatus/bestMatch.list",header=F,sep="\t",comment.char = "")
+
+# Look at the first twenty autosomes from Zebra Finch
+# https://www.ncbi.nlm.nih.gov/datasets/genome/GCF_003957565.2/
+
+autosome_list <- c("NC_044211.2","NC_044212.2","NC_044213.2","NC_044214.2",
+                   "NC_044215.2","NC_044216.2","NC_044218.2","NC_044219.2",
+                   "NC_044220.2","NC_044221.2","NC_044222.2","NC_044223.2",
+                   "NC_044224.2","NC_044225.2","NC_044226.2","NC_044227.2",
+                   "NC_044228.2","NC_044229.2","NC_044230.2","NC_044231.2")
+
+
+# Curate a list of contigs that are homologous to these autosomes in Zebra Finch
+
+matches_auto <- matches[matches$V8%in%autosome_list,]
+matches_auto$window_hits <- matches_auto$V3 - matches_auto$V2
+matches_auto_order <- matches_auto[order(matches_auto$V1),]
+matches_auto_order_nondup <- matches_auto_order[!duplicated(matches_auto_order$V1),]
+
+
+matches_auto_summary <- matches_auto %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_auto_summary <- left_join(matches_auto_summary,lengths)
+
+
+# Curate a list of contigs that are homologous to Chr 5 in Zebra Finch
+
+matches_chr5 <- matches[matches$V8=="NC_044217.2",]
+matches_chr5$window_hits <- matches_chr5$V3 - matches_chr5$V2
+matches_chr5_order <- matches_chr5[order(matches_chr5$V1),]
+matches_chr5_order_nondup <- matches_chr5_order[!duplicated(matches_chr5_order$V1),]
+
+matches_chr5_summary <- matches_chr5 %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_chr5_summary <- left_join(matches_chr5_summary,lengths)
+matches_chr5_summary$percent_match <- matches_chr5_summary$windowsum/matches_chr5_summary$V2
+
+
+# Designate a threshold for number of windows with hits or percent match
+matches_chr5_final <- matches_chr5_summary[matches_chr5_summary$windowsum>35000,]
+matches_chr5_final <- matches_chr5_summary[matches_chr5_summary$percent_match>0.10,]
+
+# Curate a list of contigs that are homologous to Z in Zebra Finch
+
+matches_Z <- matches[matches$V8=="NC_044241.2",]
+matches_Z$window_hits <- matches_Z$V3 - matches_Z$V2
+matches_Z_order <- matches_Z[order(matches_Z$V1),]
+matches_Z_order_nondup <- matches_Z_order[!duplicated(matches_Z_order$V1),]
+
+matches_Z_summary <- matches_Z %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_Z_summary <- left_join(matches_Z_summary,lengths)
+matches_Z_summary$percent_match <- round(matches_Z_summary$windowsum/matches_Z_summary$V2,3)
+
+# Designate a threshold for number of windows with hits or percent match
+matches_Z_final <- matches_Z_summary[matches_Z_summary$windowsum>30000,]
+matches_Z_final <- matches_Z_summary[matches_Z_summary$percent_match>0.06,]
+
+# Curate a list of contigs that are homologous to W in Zebra Finch
+
+matches_W <- matches[matches$V8=="NC_045028.1",]
+matches_W$window_hits <- matches_W$V3 - matches_W$V2
+matches_W_order <- matches_W[order(matches_W$V1),]
+matches_W_order_nondup <- matches_W_order[!duplicated(matches_W_order$V1),]
+
+
+matches_W_summary <- matches_W %>% select(V1,window_hits) %>%
+  group_by(V1) %>%
+  summarize(windowsum = sum(window_hits,na.rm=TRUE))%>%
+  as.data.frame()
+
+matches_W_summary <- left_join(matches_W_summary,lengths)
+matches_W_summary$percent_match <- round(matches_W_summary$windowsum/matches_W_summary$V2,3)
+
+# Designate a threshold for number of windows with hits or percent match
+matches_W_final <- matches_W_summary[matches_W_summary$windowsum>10000,]
+matches_W_final <- matches_W_summary[matches_W_summary$percent_match>0.06,]
+
+
+##### Estimate coverage for each genomic compartment
+
+
+coverage <- read.table("coverage/gencov.mismatch.0.0.out",header=F,sep="\t",comment.char = "")
+
+# Calculate median depth for selected 20 autosomes for each sex
+
+auto_coverage <- coverage[coverage$V1%in%matches_auto_order_nondup$V1,]
+
+# females
+median_sampleV4 <- median(auto_coverage$V4)
+
+# males
+median_sampleV5 <- median(auto_coverage$V5)
+
+# calculate log2FM following Schield et al. 2022
+
+coverage$Fnorm <- coverage$V4/median_sampleV4
+coverage$Mnorm <- coverage$V5/median_sampleV5
+
+coverage$log2FM <- log2(coverage$Fnorm/coverage$Mnorm)
+
+#### Chr 5 coverage calculations ####
+
+# Subset coverage based on window hits to Chr 5
+coverage_chr5 <- coverage[coverage$V1%in%matches_chr5_final$V1,]
+coverage_chr5$contig <- str_remove_all(coverage_chr5$V1,"nleucotis_yamma_366917_f#pri#ptg")
+
+
+ggplot(coverage_chr5,aes(contig,log2FM))+
+  geom_boxplot()
+
+##### outdated list of Chr 5 autosomal contigs #####
+#chr5_autosomal <- c("nleucotis_yamma_366917_f#pri#ptg000004l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000028l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000029l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000034l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000040l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000041l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000058l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000063l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000069l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000098l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000115l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000225l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000244l",
+#                    "nleucotis_yamma_366917_f#pri#ptg000406l")
+#####
+
+chr5_autosomal <- c("nleucotis_yamma_366917_f#pri#ptg000058l",
+                    "nleucotis_yamma_366917_f#pri#ptg000069l",
+                    "nleucotis_yamma_366917_f#pri#ptg000098l")
+
+chr5_autosomal_lengths <- lengths[lengths$V1%in%chr5_autosomal,]
+sum(chr5_autosomal_lengths$V2)
+
+chr5_Z <- c("nleucotis_yamma_366917_f#pri#ptg000009l",
+            "nleucotis_yamma_366917_f#pri#ptg000026l")
+
+
+chr5_Z_lengths <- lengths[lengths$V1%in%chr5_Z,]
+
+chr5_W <- c("nleucotis_yamma_366917_f#pri#ptg000039l",
+            "nleucotis_yamma_366917_f#pri#ptg000071l")
+
+chr5_W_lengths <- lengths[lengths$V1%in%chr5_W,]
+
+#### Chr Z coverage calculations ####
+
+# Subset coverage based on window hits to Chr Z
+coverage_Z <- coverage[coverage$V1%in%matches_Z_final$V1,]
+coverage_Z$contig <- str_remove_all(coverage_Z$V1,"nleucotis_yamma_366917_f#pri#ptg")
+
+ggplot(coverage_Z,aes(contig,log2FM))+
+  geom_boxplot()
+
+chrZ_hits <- c("nleucotis_yamma_366917_f#pri#ptg000025l",
+               "nleucotis_yamma_366917_f#pri#ptg000026l",
+               "nleucotis_yamma_366917_f#pri#ptg000065l",
+               "nleucotis_yamma_366917_f#pri#ptg000066l",
+               "nleucotis_yamma_366917_f#pri#ptg000072l",
+               "nleucotis_yamma_366917_f#pri#ptg000097l",
+               "nleucotis_yamma_366917_f#pri#ptg000111l",
+               "nleucotis_yamma_366917_f#pri#ptg000264l")
+
+chrZ_hits_lengths <- lengths[lengths$V1%in%chrZ_hits,]
+
+chrW_hits <- c("nleucotis_yamma_366917_f#pri#ptg000030l",
+               "nleucotis_yamma_366917_f#pri#ptg000039l",
+               "nleucotis_yamma_366917_f#pri#ptg000062l",
+               "nleucotis_yamma_366917_f#pri#ptg000071l",
+               "nleucotis_yamma_366917_f#pri#ptg000088l",
+               "nleucotis_yamma_366917_f#pri#ptg000213l")
+
+chrW_hits_lengths <- lengths[lengths$V1%in%chrW_hits,]
+
+#### Chr W coverage calculations ####
+
+# Subset coverage based on window hits to Chr W
+coverage_W <- coverage[coverage$V1%in%matches_W_final$V1,]
+coverage_W$contig <- str_remove_all(coverage_W$V1,"nleucotis_yamma_366917_f#pri#ptg")
+
+ggplot(coverage_W,aes(contig,log2FM))+
+  geom_boxplot()+
+  theme(axis.text.x=element_text(angle=45))
+
+
+# adding to ChrW_hits from above
+
+chrW_hits <- c("nleucotis_yamma_366917_f#pri#ptg000030l",
+               "nleucotis_yamma_366917_f#pri#ptg000031l",
+               "nleucotis_yamma_366917_f#pri#ptg000039l",
+               "nleucotis_yamma_366917_f#pri#ptg000062l",
+               "nleucotis_yamma_366917_f#pri#ptg000071l",
+               "nleucotis_yamma_366917_f#pri#ptg000085l",
+               "nleucotis_yamma_366917_f#pri#ptg000088l",
+               "nleucotis_yamma_366917_f#pri#ptg000090l",
+               "nleucotis_yamma_366917_f#pri#ptg000091l",
+               "nleucotis_yamma_366917_f#pri#ptg000096l",
+               "nleucotis_yamma_366917_f#pri#ptg000213l",
+               "nleucotis_yamma_366917_f#pri#ptg001422l")
+chrW_hits_lengths <- lengths[lengths$V1%in%chrW_hits,]
+sum(chrW_hits_lengths$V2)
+
+# adding to ChrZ hits from above
+chrZ_hits <- c("nleucotis_yamma_366917_f#pri#ptg000025l",
+               "nleucotis_yamma_366917_f#pri#ptg000026l",
+               "nleucotis_yamma_366917_f#pri#ptg000057l",
+               "nleucotis_yamma_366917_f#pri#ptg000065l",
+               "nleucotis_yamma_366917_f#pri#ptg000066l",
+               "nleucotis_yamma_366917_f#pri#ptg000072l",
+               "nleucotis_yamma_366917_f#pri#ptg000097l",
+               "nleucotis_yamma_366917_f#pri#ptg000111l",
+               "nleucotis_yamma_366917_f#pri#ptg000107l",
+               "nleucotis_yamma_366917_f#pri#ptg000264l")
+chrZ_hits_lengths <- lengths[lengths$V1%in%chrZ_hits,]
+sum(chrZ_hits_lengths$V2)
+
+
+#### Calculate mean depth and log2FM depth #####
+
+summary <- coverage %>% select(V1,Fnorm,Mnorm,V4,V5) %>%
+  group_by(V1) %>%
+  summarize(meanFnorm = mean(Fnorm,na.rm=TRUE),
+            meanMnorm = mean(Mnorm,na.rm=TRUE),
+            meanF = mean(V4,na.rm=TRUE),
+            meanM = mean(V5,na.rm=TRUE))%>%
+  as.data.frame()
+
+
+summary$log2FM <- log2(summary$meanF/summary$meanM)
